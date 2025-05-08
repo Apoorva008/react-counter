6d8cae2ee27cc3bd3ddc3b2156414245a2939de2
Apoorva008 <apoorvakhamele08@gmail.com>
2025-05-08 10:17:59 +0530

    fixed the issue of styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React from "react";
+import Counter from "./components/Counter";
 
 const App = () => {
   return (
-    <div>
+    <div style={styles.container}>
       <h1>Simple Counter App</h1>
       <Counter />
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+    textAlign: "center",
+    fontFamily: "Arial",
+  },
 };
 
 export default App;
